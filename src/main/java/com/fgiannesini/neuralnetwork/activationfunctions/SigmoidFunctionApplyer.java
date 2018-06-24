2f19936abb75fab957a9cfcc3c6ce0016d4c70de
fabien.giannesini <fabien.giannesini@gmail.com>
2018-06-24 22:01:59 +0200

    Add sigmoid computation remind

--- src/main/java/com/fgiannesini/neuralnetwork/activationfunctions/SigmoidFunctionApplyer.java
+++ src/main/java/com/fgiannesini/neuralnetwork/activationfunctions/SigmoidFunctionApplyer.java
@@ -7,6 +7,7 @@ public class SigmoidFunctionApplyer implements ActivationFunctionApplyer {
 
     @Override
     public FloatMatrix apply(FloatMatrix input) {
+        //1/(1+exp(-z))
         FloatMatrix output = input.mul(-1);
         output = MatrixFunctions.expi(output);
         output = output.addi(1);
