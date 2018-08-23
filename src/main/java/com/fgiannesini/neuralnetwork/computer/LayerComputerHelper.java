292133fe9293d208482ff70c938eedd0ade18859
fabien.giannesini <fabien.giannesini@gmail.com>
2018-08-23 23:07:18 +0200

    Start LayerComputerHelper for batch norm

--- src/main/java/com/fgiannesini/neuralnetwork/computer/LayerComputerHelper.java
+++ src/main/java/com/fgiannesini/neuralnetwork/computer/LayerComputerHelper.java
@@ -1,12 +1,21 @@
 package com.fgiannesini.neuralnetwork.computer;
 
 import com.fgiannesini.neuralnetwork.activationfunctions.ActivationFunctionApplier;
+import com.fgiannesini.neuralnetwork.model.BatchNormLayer;
+import com.fgiannesini.neuralnetwork.model.Layer;
 import com.fgiannesini.neuralnetwork.model.WeightBiasLayer;
 import org.jblas.DoubleMatrix;
+import org.jblas.MatrixFunctions;
 
 public class LayerComputerHelper {
 
-    public static DoubleMatrix computeAFromZ(DoubleMatrix z, WeightBiasLayer layer) {
+    private static double epsilon;
+
+    public LayerComputerHelper() {
+        epsilon = Math.pow(10, -8);
+    }
+
+    public static DoubleMatrix computeAFromZ(DoubleMatrix z, Layer layer) {
         ActivationFunctionApplier activationFunctionApplier = layer.getActivationFunctionType().getActivationFunction();
         return activationFunctionApplier.apply(z);
     }
@@ -21,4 +30,22 @@ public class LayerComputerHelper {
         return computeAFromZ(z, layer);
     }
 
+    public static DoubleMatrix computeAFromInput(DoubleMatrix input, BatchNormLayer layer) {
+        DoubleMatrix z = computeZFromInput(input, layer);
+        return computeAFromZ(z, layer);
+    }
+
+    public static DoubleMatrix computeZFromInput(DoubleMatrix input, BatchNormLayer layer) {
+        //Z1 = W.X
+        DoubleMatrix z = layer.getWeightMatrix().mmul(input);
+
+        //mean
+        DoubleMatrix means = input.rowMeans();
+
+        //sigma
+        DoubleMatrix standardDeviation = MatrixFunctions.sqrt(MatrixFunctions.pow(input.subColumnVector(means), 2).rowMeans().addi(epsilon));
+
+        //Z2 = (Z1 - mean) / sigma * gamma + beta
+        return z.subColumnVector(means).diviColumnVector(standardDeviation).muliColumnVector(layer.getGammaMatrix()).addi(layer.getBetaMatrix());
+    }
 }
--- src/test/java/com/fgiannesini/neuralnetwork/computer/LayerComputerHelperTest.java
+++ src/test/java/com/fgiannesini/neuralnetwork/computer/LayerComputerHelperTest.java
@@ -2,6 +2,7 @@ package com.fgiannesini.neuralnetwork.computer;
 
 import com.fgiannesini.neuralnetwork.activationfunctions.ActivationFunctionType;
 import com.fgiannesini.neuralnetwork.initializer.InitializerType;
+import com.fgiannesini.neuralnetwork.model.BatchNormLayer;
 import com.fgiannesini.neuralnetwork.model.WeightBiasLayer;
 import org.jblas.DoubleMatrix;
 import org.junit.jupiter.api.Assertions;
@@ -10,22 +11,29 @@ import org.junit.jupiter.api.Test;
 class LayerComputerHelperTest {
 
     @Test
-    void compute_on_one_weight() {
+    void compute_on_one_weight_and_weight_bias() {
         WeightBiasLayer layer = new WeightBiasLayer(1, 1, InitializerType.ONES.getInitializer(), ActivationFunctionType.NONE);
         DoubleMatrix output = LayerComputerHelper.computeAFromInput(DoubleMatrix.scalar(3f), layer);
         Assertions.assertArrayEquals(new double[]{4}, output.data);
     }
 
     @Test
-    void compute_on_weight_array() {
+    void compute_on_weight_array_and_weight_bias() {
         WeightBiasLayer layer = new WeightBiasLayer(5, 1, InitializerType.ONES.getInitializer(), ActivationFunctionType.NONE);
         DoubleMatrix output = LayerComputerHelper.computeAFromInput(DoubleMatrix.ones(5).mul(3f), layer);
         Assertions.assertArrayEquals(new double[]{16}, output.data);
     }
 
     @Test
-    void compute_on_weight_matrix() {
+    void compute_on_weight_matrix_and_weight_bias() {
         WeightBiasLayer layer = new WeightBiasLayer(5, 2, InitializerType.ONES.getInitializer(), ActivationFunctionType.NONE);
+        DoubleMatrix output = LayerComputerHelper.computeAFromInput(DoubleMatrix.ones(5).mul(3f), layer);
+        Assertions.assertArrayEquals(new double[]{16, 16}, output.data);
+    }
+
+    @Test
+    void compute_on_weight_matrix_and_batch_norm() {
+        BatchNormLayer layer = new BatchNormLayer(5, 2, InitializerType.ONES.getInitializer(), ActivationFunctionType.NONE);
         DoubleMatrix output = LayerComputerHelper.computeAFromInput(DoubleMatrix.ones(5).mul(3f), layer);
         Assertions.assertArrayEquals(new double[]{16, 16}, output.data);
     }
